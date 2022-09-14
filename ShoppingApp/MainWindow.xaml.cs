eaf585f4422384cb77d7871f8e921bf7d3ad315d
Bayr_kf60@itstep.edu.az <Bayr_kf60@itstep.edu.az>
2022-09-14 07:44:06 -0700

    Upgraded design

--- ShoppingApp/MainWindow.xaml.cs
+++ ShoppingApp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,135 @@ namespace ShoppingApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Product> Products { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            Products = GetProducts();
+            this.DataContext = this;
+        }
+
+
+
+        public List<Product> GetProducts()
+        {
+            return new List<Product>
+            {
+                new Product
+                {
+                     ImagePath="Images/Products/product1.png",
+                     ProductName ="Amsterdam eBike",
+                     ProductPrice = 2698
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product2.png",
+                     ProductName ="Hoodie",
+                     ProductPrice = 22.12
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product3.png",
+                     ProductName ="428 HDR Heavy",
+                     ProductPrice = 49.88
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product4.png",
+                     ProductName ="Luxury Bus seating",
+                     ProductPrice = 825.7
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product5.png",
+                     ProductName ="Fuel Off-Road",
+                     ProductPrice = 214.5
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product7.png",
+                     ProductName ="T-shirt",
+                     ProductPrice = 12
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product6.png",
+                     ProductName ="Sour Bicycles",
+                     ProductPrice = 298
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product8.png",
+                     ProductName ="DJI Mavic 3",
+                     ProductPrice = 1929
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product9.png",
+                     ProductName ="Iphone 13 Pro Max",
+                     ProductPrice = 1099.99
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product10.png",
+                     ProductName ="Asus Zenbook Pro",
+                     ProductPrice = 2234.89
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product1.png",
+                     ProductName ="Amsterdam eBike",
+                     ProductPrice = 2698
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product1.png",
+                     ProductName ="Amsterdam eBike",
+                     ProductPrice = 2698
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product1.png",
+                     ProductName ="Amsterdam eBike",
+                     ProductPrice = 2698
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product1.png",
+                     ProductName ="Amsterdam eBike",
+                     ProductPrice = 2698
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product1.png",
+                     ProductName ="Amsterdam eBike",
+                     ProductPrice = 2698
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product1.png",
+                     ProductName ="Amsterdam eBike",
+                     ProductPrice = 2698
+                },
+                new Product
+                {
+                     ImagePath="Images/Products/product1.png",
+                     ProductName ="Amsterdam eBike",
+                     ProductPrice = 2698
+                },
+                  new Product
+                {
+                     ImagePath="Images/Products/product1.png",
+                     ProductName ="Amsterdam eBike",
+                     ProductPrice = 2698
+                },
+                    new Product
+                {
+                     ImagePath="Images/Products/product1.png",
+                     ProductName ="Amsterdam eBike",
+                     ProductPrice = 2698
+                },
+            };
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
@@ -41,6 +169,20 @@ namespace ShoppingApp
         private void CloseBtn_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+        public bool IsFullScreen { get; set; }
+        private void maximizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsFullScreen)
+            {
+                WindowState = WindowState.Normal;
+                IsFullScreen = false;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+                IsFullScreen = true;
+            }
         }
     }
 }
---
+++ ShoppingApp/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp
+{
+    public class Product
+    {
+        private string imagePath;
+
+        public string ImagePath
+        {
+            get { return imagePath; }
+            set { imagePath = value; }
+        }
+        private string prodyctName;
+
+        public string ProductName
+        {
+            get { return prodyctName; }
+            set { prodyctName = value; }
+        }
+        private double productPrice;
+
+        public double ProductPrice
+        {
+            get { return productPrice; }
+            set { productPrice = value; }
+        }
+
+
+    }
+}
